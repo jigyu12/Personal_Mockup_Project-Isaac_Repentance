e560bf9e106b90d7527a0d4775e6b83bc9c49afa
민지규 <jigyu12@gmail.com>
2024-11-18 22:59:13 +0900

    Modify ObjectPool.h
    1. automatically calls the AddGo, RemoveGo
    2. Add Exit, Release AllUnUsedObject func

--- Personal Mockup Project-Isaac Repentance/Framework/ObjectPool.h
+++ Personal Mockup Project-Isaac Repentance/Framework/ObjectPool.h
@@ -1,39 +1,19 @@
 #pragma once
 
 /// <summary>
-/// This class automatically calls the AddGo function of the Scene class.
+/// This class automatically calls the AddGo, RemoveGo function of the Scene class.
 /// If you use object pooling to create a specific object, do not create it in any other way.
+/// Do not use this class as a local variable. use it as a member variable instead.
+/// Note that when the Scene's Exit and Release functions are called, the Exit and Release of the object pool must also be called.
 /// </summary>
 /// <typeparam name="T"></typeparam>
 template<typename T>
 class ObjectPool
 {
 public:
-	ObjectPool(int initSize = 10)
-	{
-		for (int i = 0; i < initSize; i++)
-		{
-			auto objectPtr = std::make_shared<T>();
+	ObjectPool() = default;
 
-			objectPtr->Init();
-
-			unusedList.push_back(objectPtr);
-		}
-	}
-
-	~ObjectPool()
-	{
-		for (auto& unusedObject : unusedList)
-		{
-			unusedObject->Exit();
-			unusedObject->Release();
-		}
-		for (auto& usedObject : usedList)
-		{
-			usedObject->Exit();
-			usedObject->Release();
-		}
-	}
+	~ObjectPool() = default;
 
 	std::shared_ptr<T> Take(std::list<std::shared_ptr<GameObject>>& addGameObjects)
 	{
@@ -63,7 +43,7 @@ public:
 		return objectPtr;
 	}
 
-	void Return(std::shared_ptr<T> objectPtr)
+	void Return(std::list<std::shared_ptr<GameObject>>& removeGameObjects, std::shared_ptr<T> objectPtr)
 	{
 		auto findIt = std::find(usedList.begin(), usedList.end(), objectPtr);
 		if (findIt == usedList.end())
@@ -73,10 +53,24 @@ public:
 			return;
 		}
 
+		removeGameObjects.push_back(objectPtr);
+
 		usedList.erase(findIt);
 
 		unusedList.push_back(objectPtr);
 		objectPtr->SetActive(false);
+	}
+
+	void ExitAllUnUsedObject()
+	{
+		for (auto& unusedObject : unusedList)
+			unusedObject->Exit();
+	}
+
+	void ReleaseAllUnUsedObject()
+	{
+		for (auto& unusedObject : unusedList)
+			unusedObject->Release();
 	}
 
 private:
